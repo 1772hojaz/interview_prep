f5d096e5253d5252c728fa4fa424ab590fb41ded
Humphrey Nyahoja <h.nyahoja@alustudent.com>
2026-07-26 18:25:48 +0200

    added linear seach

--- src/arrays/basic/search_element.py
+++ src/arrays/basic/search_element.py
@@ -2,14 +2,30 @@
 """Search Element"""
 
 
-def search_element(arr):
+def search_element(element,arr):
     """Search Element.
     
     Args:
+        element: The element being searched for
         arr: List of integers
         
     Returns:
         Result based on problem description
     """
-    pass
+    for i in range(0,len(arr), 1):
+        if element == arr[i]:
+            print(f"{element} found")
+            return 1
+        
+    print("element not found")
+    return 0
+
+
+            
+
+a = [1,2,3,4,5]
+l = 5
+x = 6
+print(search_element(l,a))
+print(search_element(x,a))
 
